a5ea8640dda6088c93120f95bf5f70aefd24a11c
Sergei Kolesnikov <win0err@gmail.com>
2017-06-28 06:55:13 +0300

    Работа над ранжированием

--- src/Finder.php
+++ src/Finder.php
@@ -32,19 +32,29 @@ class Finder {
 
 	public function find(Query $query) {
 
-		$words = self::explode( addslashes( $query->getText() ) );
-
-		// ToDo: Вынести блэклист в отдельный класс
-		$words = array_diff( $words, static::$blacklist );
+		$words = self::explode( self::clearHTML( $query->getText() ) );
 
 		$mp = $this->morphologyProcessor;
-		$words = array_map(
-			function ($word) use ($mp) {
+		$applyMp = function ($word) use ($mp) {
 
-				return $mp::apply( (string)$word );
-			}, $words );
+			return $mp::apply( (string)$word );
+		};
+		$words = array_map( $applyMp, $words );
 
-		return $this->storage->getIndexablesByWords( $words );
+		$queryWordsCount = sizeof( $words );
+
+		// ToDo: Вынести блэклист в отдельный класс
+		//$words = array_diff( $words, static::$blacklist );
+
+		foreach ($this->storage->getIndexablesByWords( $words ) as $indexable) {
+
+			$titleByWords = array_map( $applyMp, self::explode( self::clearHTML( $indexable->getTitle() ) ) );
+			$newRating = $indexable->getRating() * (1 + 4 * sizeof( array_intersect( $words, $titleByWords ) ) / $queryWordsCount);
+
+			$indexable->setRating($newRating);
+
+			yield $indexable;
+		}
 	}
 
 	/**
@@ -60,5 +70,37 @@ class Finder {
 	protected static function explode($contents) {
 
 		return mb_split( '[\s-]+', $contents );
+	}
+
+
+	/**
+	 * Очищает HTML-код
+	 *
+	 * ToDo: Убрать дублирование в Indexer
+	 *
+	 * @param string $html
+	 *
+	 * @return string
+	 */
+	protected static function clearHTML(string $html): string {
+
+		// Чистим от HTML-тегов и переводим в нижний регистр
+		$html = strip_tags( mb_strtolower( $html ) );
+
+		// Символ неразрывного пробела
+		$html = str_replace( ['&nbsp;', "\xc2\xa0"], ' ', $html );
+
+		// ё → е
+		$html = str_replace( 'ё', 'е', $html );
+
+		// HTML-коды: &lt; or &#60;
+		$html = mb_ereg_replace( '&[^;]{1,20};', '', $html );
+
+		// Оставляем разрешённые символы
+		$html = mb_ereg_replace( '[^\\-а-яё0-9a-z\\^_]+', ' ', $html );
+
+		$html = trim( $html );
+
+		return $html;
 	}
 }
